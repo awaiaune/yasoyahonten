ce0b9392b926ca7516e1f25a5e8c588898fa0fa0
awaiaune <tatsurou3523@gmail.com>
2026-08-04 14:03:15 +0900

    Update catalog.js

--- worker/src/catalog.js
+++ worker/src/catalog.js
@@ -9,21 +9,21 @@ export const CATALOG = Object.freeze({
         name: "乾燥セイタカアワダチソウ 10g",
         price: 980,
         active: true,
-        maxQuantity: 3
+        maxQuantity: 10
     }),
 
     "lemonbalm-10g": Object.freeze({
         name: "乾燥レモンバーム 10g",
         price: 980,
         active: true,
-        maxQuantity: 3
+        maxQuantity: 10
     }),
 
     "yomogi-10g": Object.freeze({
         name: "乾燥ヨモギ 10g",
         price: 980,
         active: true,
-        maxQuantity: 3
+        maxQuantity: 10
     })
 });
 
