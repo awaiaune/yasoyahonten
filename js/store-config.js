f25386b3e54738cfe292f2c2d9ffb1b8970a6058
awaiaune <tatsurou3523@gmail.com>
2026-08-28 20:17:53 +0900

    Update store-config.js

--- js/store-config.js
+++ js/store-config.js
@@ -1,9 +1,9 @@
 window.YASOYA_STORE_CONFIG = Object.freeze({
-    environment: "sandbox",
+    environment: "production",
 
     // 公開可能なIDです。Sandbox Credentials画面の値へ置換してください。
-    squareApplicationId: "sandbox-sq0idb-HLcXmwmBqDL1WpF0G5eQEw",
-    squareLocationId: "LHD4ZZ1ZYPHD1",
+    squareApplicationId: "sq0idp-JYrGicMfKr-ILhZeLS9Xsw",
+    squareLocationId: "LQ693S4H2NBZ8",
 
     // npx wrangler deploy 後に表示されたURLへ置換してください。
     workerBaseUrl: "https://yasoya-honten-payments.tatsurou3523.workers.dev",
